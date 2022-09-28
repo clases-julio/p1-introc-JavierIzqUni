5dc53ece80581c9a35e0f81cfd488c9b6fad9e1b
Javier Izquierdo Hernandez <j.izquierdoh.2021@alumnos.urjc.es>
2022-09-28 14:33:23 +0200

    " Ejercicio 1 with double imput "

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -11,9 +11,8 @@ substract, multiplication and division of them.
 using namespace std;
 
 int main() { // function main begins program execution
-  int num1, num2;
-  int sum , dif, mult;
-  double div;
+  double num1, num2;
+  double sum , dif, mult, div;
   cout << "Introduce two numbers\n"; // show message
   cout << "Introduce one: ";
   cin >> num1;
@@ -22,19 +21,19 @@ int main() { // function main begins program execution
 
   // Sum and print the result in the terminal
   sum = num1 +num2;
-  cout << "\nThe result of " << num1 << " plus " << num2 << " is:" << sum << endl;
+  cout << "\nThe result of " << num1 << " plus " << num2 << " is: " << sum << endl;
 
   // Multiply and print the result in the terminal
   mult = num1 * num2;
-  cout << "The result of " << num1 << " multiply by " << num2 << " is:" << mult << endl;
+  cout << "The result of " << num1 << " multiply by " << num2 << " is: " << mult << endl;
   
   // Substract and print the result in the terminal
   dif = num1 - num2;
-  cout << "The result of " << num1 << " minus " << num2 << " is:" << dif << endl;
+  cout << "The result of " << num1 << " minus " << num2 << " is: " << dif << endl;
 
   // Divide and print the result in the terminal.
-  div = num1 / (double)num2; // You need to cast to a double to obtain the accurate result
-  cout << "The result of " << num1 << " divided by" << num2 << " is:" << div << endl;
+  div = num1 / num2;
+  cout << "The result of " << num1 << " divided by" << num2 << " is: " << div << endl;
 
   // End of program
   return 0;
