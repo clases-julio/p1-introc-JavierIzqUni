97337b69e1e693f0e412ff194f73e703df27ece9
Javier Izquierdo Hernandez <j.izquierdoh.2021@alumnos.urjc.es>
2022-09-29 13:14:25 +0200

    "Adding excercise 2"

---
+++ ejercicio2b.cpp
@@ -0,0 +1,18 @@
+/* ---------------------------
+File: ejercicio2b.cpp
+Author: Javier Izquierdo
+Date: 29/09/22
+Goal: program that prints numbers from 1 to 4
+---------------------------- */
+
+#include <iostream>
+
+using namespace std;
+
+int main() { // function main begins program execution
+  
+  cout << "1 " << "2 " << "3 " << "4\n"; // show message
+
+  // End of program
+  return 0;
+}
---
+++ ejercicio2c.cpp
@@ -0,0 +1,21 @@
+/* ---------------------------
+File: ejercicio2c.cpp
+Author: Javier Izquierdo
+Date: 29/09/22
+Goal: program that prints numbers from 1 to 4
+---------------------------- */
+
+#include <iostream>
+
+using namespace std;
+
+int main() { // function main begins program execution
+  
+  cout << "1 "; // show message
+  cout << "2 ";
+  cout << "3 ";
+  cout << "4\n";
+
+  // End of program
+  return 0;
+}
