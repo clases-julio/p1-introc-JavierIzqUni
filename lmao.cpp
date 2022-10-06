4a329c1ac396c9360ac00a4dc095b09085e21e1e
dgarciac2021 <92941081+dgarciac2021@users.noreply.github.com>
2022-10-06 12:52:14 +0200

    Interesting!
    Would you like to share your code with the class? Very kind of you!

---
+++ lmao.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+* File: lmao.cpp                                                         *
+* Author: Diego Garcia                                                   *
+* Date: 06/10/2022                                                       *
+* Goal: This program will do absolute nothing, tbh.                      *
+* Version: 0.0                                                           *
+*************************************************************************/
+
+#include <iostream>
+
+int main(){
+  std::cout << "Hello world from C plas plas!";
+  
+  return 0;
+}
